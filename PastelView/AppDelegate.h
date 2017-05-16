c13ba7ea6fffd7cd9d1be83a44ef135858937ac4
angelen <iby2girls@gmail.com>
2017-05-16 16:22:40 +0800

    Initial Commit

---
+++ PastelView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PastelView
+//
+//  Created by angelen on 2017/5/16.
+//  Copyright © 2017年 angelen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PastelView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PastelView
+//
+//  Created by angelen on 2017/5/16.
+//  Copyright © 2017年 angelen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
